fff811ab6a217819a8eba62041fe1adae0244e9f
Patricia Medina <f.patricia.medina@gmail.com>
2021-10-15 18:03:11 -0500

    desplegar propiedad de película

--- src/peliculas/listado.js
+++ src/peliculas/listado.js
@@ -1,14 +1,21 @@
+import { inject, observer } from "mobx-react"
 import React from "react"
 import {Link} from 'react-router-dom'
 class ListadoPeliculas extends React.Component
 {
     render()
     {
+        const {peliculas}= this.props
+
         return(  //siempre paréntesis para que no se confunda el compilador
             <div>listado
+                <h1>Listado de Películas</h1>
+                <h2>{peliculas.nombre}</h2>
                 <Link to="/">regresar a home</Link>
             </div>
         )
     }
 }
-export default ListadoPeliculas
+export default inject('peliculas')(
+    observer(ListadoPeliculas)
+)
